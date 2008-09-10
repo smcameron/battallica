c56e20bb981cddc1c551171dd578287304f7ddf2
Stephen M. Cameron <smcameron@yahoo.com>
2008-09-10 17:32:05 -0500

    Add some code to deal with the keyboard.

--- battallica.c
+++ battallica.c
@@ -29,6 +29,8 @@
 #include <limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <gdk/gdkkeysyms.h>
+
 
 #define SCREEN_WIDTH 800        /* window width, in pixels */
 #define SCREEN_HEIGHT 600       /* window height, in pixels */
@@ -179,6 +181,212 @@ static int main_da_expose(GtkWidget *w, GdkEvent *event, gpointer p)
 	return 0;
 }
 
+/**********************************/
+/* keyboard handling stuff begins */
+
+struct keyname_value_entry {
+	char *name;
+	unsigned int value;
+} keyname_value_map[] = {
+	{ "a", GDK_a }, 
+	{ "b", GDK_b }, 
+	{ "c", GDK_c }, 
+	{ "d", GDK_d }, 
+	{ "e", GDK_e }, 
+	{ "f", GDK_f }, 
+	{ "g", GDK_g }, 
+	{ "h", GDK_h }, 
+	{ "i", GDK_i }, 
+	{ "j", GDK_j }, 
+	{ "k", GDK_k }, 
+	{ "l", GDK_l }, 
+	{ "m", GDK_m }, 
+	{ "n", GDK_n }, 
+	{ "o", GDK_o }, 
+	{ "p", GDK_p }, 
+	{ "q", GDK_q }, 
+	{ "r", GDK_r }, 
+	{ "s", GDK_s }, 
+	{ "t", GDK_t }, 
+	{ "u", GDK_u }, 
+	{ "v", GDK_v }, 
+	{ "w", GDK_w }, 
+	{ "x", GDK_x }, 
+	{ "y", GDK_y }, 
+	{ "z", GDK_z }, 
+	{ "A", GDK_A }, 
+	{ "B", GDK_B }, 
+	{ "C", GDK_C }, 
+	{ "D", GDK_D }, 
+	{ "E", GDK_E }, 
+	{ "F", GDK_F }, 
+	{ "G", GDK_G }, 
+	{ "H", GDK_H }, 
+	{ "I", GDK_I }, 
+	{ "J", GDK_J }, 
+	{ "K", GDK_K }, 
+	{ "L", GDK_L }, 
+	{ "M", GDK_M }, 
+	{ "N", GDK_N }, 
+	{ "O", GDK_O }, 
+	{ "P", GDK_P }, 
+	{ "Q", GDK_Q }, 
+	{ "R", GDK_R }, 
+	{ "S", GDK_S }, 
+	{ "T", GDK_T }, 
+	{ "U", GDK_U }, 
+	{ "V", GDK_V }, 
+	{ "W", GDK_W }, 
+	{ "X", GDK_X }, 
+	{ "Y", GDK_Y }, 
+	{ "Z", GDK_Z }, 
+	{ "0", GDK_0 }, 
+	{ "1", GDK_1 }, 
+	{ "2", GDK_2 }, 
+	{ "3", GDK_3 }, 
+	{ "4", GDK_4 }, 
+	{ "5", GDK_5 }, 
+	{ "6", GDK_6 }, 
+	{ "7", GDK_7 }, 
+	{ "8", GDK_8 }, 
+	{ "9", GDK_9 }, 
+	{ "-", GDK_minus }, 
+	{ "+", GDK_plus }, 
+	{ "=", GDK_equal }, 
+	{ "?", GDK_question }, 
+	{ ".", GDK_period }, 
+	{ ",", GDK_comma }, 
+	{ "<", GDK_less }, 
+	{ ">", GDK_greater }, 
+	{ ":", GDK_colon }, 
+	{ ";", GDK_semicolon }, 
+	{ "@", GDK_at }, 
+	{ "*", GDK_asterisk }, 
+	{ "$", GDK_dollar }, 
+	{ "%", GDK_percent }, 
+	{ "&", GDK_ampersand }, 
+	{ "'", GDK_apostrophe }, 
+	{ "(", GDK_parenleft }, 
+	{ ")", GDK_parenright }, 
+	{ "space", GDK_space }, 
+	{ "enter", GDK_Return }, 
+	{ "return", GDK_Return }, 
+	{ "backspace", GDK_BackSpace }, 
+	{ "delete", GDK_Delete }, 
+	{ "pause", GDK_Pause }, 
+	{ "scrolllock", GDK_Scroll_Lock }, 
+	{ "escape", GDK_Escape }, 
+	{ "sysreq", GDK_Sys_Req }, 
+	{ "left", GDK_Left }, 
+	{ "right", GDK_Right }, 
+	{ "up", GDK_Up }, 
+	{ "down", GDK_Down }, 
+	{ "kp_home", GDK_KP_Home }, 
+	{ "kp_down", GDK_KP_Down }, 
+	{ "kp_up", GDK_KP_Up }, 
+	{ "kp_left", GDK_KP_Left }, 
+	{ "kp_right", GDK_KP_Right }, 
+	{ "kp_end", GDK_KP_End }, 
+	{ "kp_delete", GDK_KP_Delete }, 
+	{ "kp_insert", GDK_KP_Insert }, 
+	{ "home", GDK_Home }, 
+	{ "down", GDK_Down }, 
+	{ "up", GDK_Up }, 
+	{ "left", GDK_Left }, 
+	{ "right", GDK_Right }, 
+	{ "end", GDK_End }, 
+	{ "delete", GDK_Delete }, 
+	{ "insert", GDK_Insert }, 
+	{ "kp_0", GDK_KP_0 }, 
+	{ "kp_1", GDK_KP_1 }, 
+	{ "kp_2", GDK_KP_2 }, 
+	{ "kp_3", GDK_KP_3 }, 
+	{ "kp_4", GDK_KP_4 }, 
+	{ "kp_5", GDK_KP_5 }, 
+	{ "kp_6", GDK_KP_6 }, 
+	{ "kp_7", GDK_KP_7 }, 
+	{ "kp_8", GDK_KP_8 }, 
+	{ "kp_9", GDK_KP_9 }, 
+	{ "f1", GDK_F1 }, 
+	{ "f2", GDK_F2 }, 
+	{ "f3", GDK_F3 }, 
+	{ "f4", GDK_F4 }, 
+	{ "f5", GDK_F5 }, 
+	{ "f6", GDK_F6 }, 
+	{ "f7", GDK_F7 }, 
+	{ "f9", GDK_F9 }, 
+	{ "f9", GDK_F9 }, 
+	{ "f10", GDK_F10 }, 
+	{ "f11", GDK_F11 }, 
+	{ "f12", GDK_F12 }, 
+};
+
+enum keyaction { keynone, keydown, keyup, keyleft, keyright, 
+		keylaser, keytransform, 
+		keyquarter, keypause, key2, key3, key4, key5, key6,
+		key7, key8, keysuicide, keyfullscreen, keythrust, 
+		keysoundeffects, keymusic, keyquit, keytogglemissilealarm,
+		keypausehelp, keyreverse
+};
+
+enum keyaction keymap[256];
+enum keyaction ffkeymap[256];
+unsigned char *keycharmap[256];
+
+char *keyactionstring[] = {
+	"none", "down", "up", "left", "right", 
+	"laser", "bomb", "chaff", "gravitybomb",
+	"quarter", "pause", "2x", "3x", "4x", "5x", "6x",
+	"7x", "8x", "suicide", "fullscreen", "thrust", 
+	"soundeffect", "music", "quit", "missilealarm", "help", "reverse"
+};
+void init_keymap()
+{
+	memset(keymap, 0, sizeof(keymap));
+	memset(ffkeymap, 0, sizeof(ffkeymap));
+	memset(keycharmap, 0, sizeof(keycharmap));
+
+	keymap[GDK_j] = keydown;
+	ffkeymap[GDK_Down & 0x00ff] = keydown;
+
+	keymap[GDK_k] = keyup;
+	ffkeymap[GDK_Up & 0x00ff] = keyup;
+
+	keymap[GDK_l] = keyright;
+	ffkeymap[GDK_Right & 0x00ff] = keyright;
+	keymap[GDK_period] = keyright;
+	keymap[GDK_greater] = keyright;
+
+	keymap[GDK_h] = keyleft;
+	ffkeymap[GDK_Left & 0x00ff] = keyleft;
+	keymap[GDK_comma] = keyleft;
+	keymap[GDK_less] = keyleft;
+
+	keymap[GDK_space] = keylaser;
+	keymap[GDK_z] = keylaser;
+
+	keymap[GDK_b] = keytransform;
+	keymap[GDK_x] = keythrust;
+	keymap[GDK_p] = keypause;
+	ffkeymap[GDK_F1 & 0x00ff] = keypausehelp;
+	keymap[GDK_q] = keyquarter;
+	keymap[GDK_m] = keymusic;
+	keymap[GDK_s] = keysoundeffects;
+	ffkeymap[GDK_Escape & 0x00ff] = keyquit;
+	keymap[GDK_1] = keytogglemissilealarm;
+
+	keymap[GDK_2] = key2;
+	keymap[GDK_3] = key3;
+	keymap[GDK_4] = key4;
+	keymap[GDK_5] = key5;
+	keymap[GDK_6] = key6;
+	keymap[GDK_7] = key7;
+	keymap[GDK_8] = key8;
+	keymap[GDK_9] = keysuicide;
+
+	ffkeymap[GDK_F11 & 0x00ff] = keyfullscreen;
+}
+
 static gint key_press_cb(GtkWidget* widget, GdkEventKey* event, gpointer data)
 {
 	return FALSE;
@@ -188,6 +396,9 @@ static gint key_release_cb(GtkWidget* widget, GdkEventKey* event, gpointer data)
 {
 	return FALSE;
 }
+
+/* keyboard handling stuff ends */
+/**********************************/
 
 /* call back for configure_event (for window resize) */
 static gint main_da_configure(GtkWidget *w, GdkEventConfigure *event)
@@ -273,6 +484,8 @@ int main(int argc, char *argv[])
 
 	gtk_set_locale();
 	gtk_init (&argc, &argv);
+
+	init_keymap();
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);		
 	gtk_container_set_border_width (GTK_CONTAINER (window), 0);
